a7af0d6ec6069af7bbb46157f6bed474893b4a83
NickMercer <dekori217@gmail.com>
2022-03-11 15:20:53 -0800

    Initial Commit

---
+++ WordTransposer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if(args.Length == 0)
+        {
+            throw new ArgumentException("Word Transposer requires a single argument that contains a file or folder path to one or more .txt files.");
+        }
+
+        var pathArgument = args[0];
+
+
+    }
+}
