f204da25b8fbafb521dab6ac8c236be3e24bf2a1
NickMercer <dekori217@gmail.com>
2022-03-12 10:59:10 -0800

    Transpose Tests

--- WordTransposer/TranspositionService.cs
+++ WordTransposer/TranspositionService.cs
@@ -8,12 +8,23 @@ namespace WordTransposer;
 public static class TranspositionService
 {
     /// <summary>
-    /// Reverses the given word and returns it
+    /// Reverses the given word and returns it. 
+    /// Note: Cannot reverse strings with Emoji in them correctly. Limitation in C#'s array reverse function.
     /// </summary>
     /// <param name="word">word to transpose</param>
     /// <returns>transposed word</returns>
     public static string Transpose(string word)
     {
+        if(word == null)
+        {
+            return null;
+        }
+
+        if(string.IsNullOrWhiteSpace(word))
+        {
+            return string.Empty;
+        }
+
         var wordChars = word.ToCharArray();
         Array.Reverse(wordChars);
         return new string(wordChars);
---
+++ WordTransposerTests/UnitTests/TranspositionServiceTests/TransposeTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using WordTransposer;
+using Xunit;
+using FluentAssertions;
+
+namespace WordTransposerTests.UnitTests.TranspositionServiceTests;
+
+public class TransposeTests
+{
+    [Theory]
+    [InlineData("desserts", "stressed")]
+    [InlineData("live", "evil")]
+    [InlineData("pupils", "slipup")]
+    [InlineData("we'd", "d'ew")]
+    [InlineData("12345", "54321")]
+    [InlineData("$%&'()*+", "+*)('&%$")]
+    [InlineData("äÙÌⓩℬ", "ℬⓩÌÙä")]
+    public void LowercaseString_TransposesCorrectly(string word, string expected)
+    {
+        //Arrange
+        //Passed in Argument
+
+        //Act
+        var actual = TranspositionService.Transpose(word);
+
+        //Assert
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("Desserts", "stresseD")]
+    [InlineData("liVE", "EVil")]
+    [InlineData("PuPils", "sliPuP")]
+    [InlineData("wE'd", "d'Ew")]
+    public void StringWithCapitals_TransposesCorrectly(string word, string expected)
+    {
+        //Arrange
+        //Passed in Argument
+
+        //Act
+        var actual = TranspositionService.Transpose(word);
+
+        //Assert
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void EmptyString_ReturnsEmptyString()
+    {
+        //Arrange
+        var word = string.Empty;
+
+        //Act
+        var actual = TranspositionService.Transpose(word);
+
+        //Assert
+        actual.Should().Be(string.Empty);
+    }
+
+    [Fact]
+    public void NullString_ReturnsNullString()
+    {
+        //Arrange
+        string word = null;
+
+        //Act
+        var actual = TranspositionService.Transpose(word);
+
+        //Assert
+        actual.Should().BeNull();
+    }
+
+    //
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData("\n")]
+    [InlineData("\r\n")]
+    public void StringIsWhiteSpace_ReturnsEmptyString(string word)
+    {
+        //Arrange
+        //Passed by argument
+
+        //Act
+        var actual = TranspositionService.Transpose(word);
+
+        //Assert
+        actual.Should().Be(string.Empty);
+    }
+}
